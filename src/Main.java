e21dc3d7476e5e44bc8d134a6b631571658a9e76
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-02-25 03:46:50 +0530

    We will focus on this.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java 8 Features.");
+        System.out.println("We will focus on these:- " +
+                "\nLambda Expressions\n" +
+                "Functional Interfaces\n" +
+                "Method Reference\n" +
+                "Streams\n" +
+                "Comparable and Comparator\n" +
+                "Optional Class\n" +
+                "Date/Time API\n" +
+                "Miscellaneous");
     }
 }
